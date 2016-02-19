da9cff24158d2f5d796250d7e7282c8eb7a9812c
Sophfia <anguianosofia@msn.com>
2016-02-19 13:00:19 -0800

    code to show fields

--- wp-content/themes/Square-Child/single-product.php
+++ wp-content/themes/Square-Child/single-product.php
@@ -21,6 +21,13 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
+			<?php echo types_render_field("product-title", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2")); ?>
+
+			<?php echo types_render_field("default-image", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2")); ?>
+
+			<?php echo types_render_field("product-description", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2")); ?>
+			
+			<?php echo types_render_field("product-dimensions", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2")); ?>
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
